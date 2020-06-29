73fb44d20056052821e491f54e6a4166df6731e8
anonsachin <sachin94roxz@gmail.com>
2020-06-29 19:33:11 +0530

    user and client certs and directory config done

--- network/certs.sh
+++ network/certs.sh
@@ -159,3 +159,51 @@ cp $FABRIC_CA_CLIENT_HOME/tls/ca.crt $PWD/certs/peerorg/tlsca/tls-ca.crt
 echo "editing root ca directory"
 mkdir $PWD/certs/peerorg/ca
 cp $FABRIC_CA_CLIENT_HOME/msp/cacerts/* $PWD/certs/peerorg/ca/peerorg-ca.cert
+
+echo "####### ENROLLING PEER ADMIN #########"
+
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/org-ca/ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=$PWD/certs/peerorg/admin
+export FABRIC_CA_CLIENT_MSPDIR=msp
+set -x
+fabric-ca-client enroll -d -u https://admin-org:adminpw@0.0.0.0:7055
+set +x
+
+echo "moving the config file"
+cp $PWD/certs/peerorg/msp/config.yaml $FABRIC_CA_CLIENT_HOME/msp/config.yaml
+
+echo "changing the root ca"
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/tls-ca/ca-cert.pem
+
+export FABRIC_CA_CLIENT_MSPDIR=tls
+set -x
+fabric-ca-client enroll -d -u https://admin-org:adminpw@0.0.0.0:7054 --enrollment.profile tls --csr.hosts admin-org --csr.hosts localhost
+set +x
+
+cp  $FABRIC_CA_CLIENT_HOME/tls/tlscacerts/* $FABRIC_CA_CLIENT_HOME/tls/ca.crt
+cp  $FABRIC_CA_CLIENT_HOME/tls/signcerts/* $FABRIC_CA_CLIENT_HOME/tls/server.crt
+cp  $FABRIC_CA_CLIENT_HOME/tls/keystore/* $FABRIC_CA_CLIENT_HOME/tls/server.key
+
+echo "####### ENROLLING PEER CLIENT #########"
+
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/org-ca/ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=$PWD/certs/peerorg/client
+export FABRIC_CA_CLIENT_MSPDIR=msp
+set -x
+fabric-ca-client enroll -d -u https://user-org:clientpw@0.0.0.0:7055
+set +x
+
+echo "moving the config file"
+cp $PWD/certs/peerorg/msp/config.yaml $FABRIC_CA_CLIENT_HOME/msp/config.yaml
+
+echo "changing the root ca"
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/tls-ca/ca-cert.pem
+
+export FABRIC_CA_CLIENT_MSPDIR=tls
+set -x
+fabric-ca-client enroll -d -u https://user-org:clientpw@0.0.0.0:7054 --enrollment.profile tls --csr.hosts client-org --csr.hosts localhost
+set +x
+
+cp  $FABRIC_CA_CLIENT_HOME/tls/tlscacerts/* $FABRIC_CA_CLIENT_HOME/tls/ca.crt
+cp  $FABRIC_CA_CLIENT_HOME/tls/signcerts/* $FABRIC_CA_CLIENT_HOME/tls/server.crt
+cp  $FABRIC_CA_CLIENT_HOME/tls/keystore/* $FABRIC_CA_CLIENT_HOME/tls/server.key

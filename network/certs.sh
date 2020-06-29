44de719434b0ecde6d30318ed122a0402d3589ef
anonsachin <sachin94roxz@gmail.com>
2020-06-29 18:57:18 +0530

    orderer setup done

--- network/certs.sh
+++ network/certs.sh
@@ -2,8 +2,8 @@
 
 if [ ! -d certs ]; then
 	mkdir -p certs/admin/tls-ca/
-	mkdir -p certs/admin/org-ca/
-	mkdir -p certs/admin/ord-ca/
+	mkdir -p certs/peerorg/
+	mkdir -p certs/ordererorg/
 	mkdir -p certs/peerorg/admin
 	mkdir -p certs/peerorg/peer
 	mkdir -p certs/peerorg/client
@@ -37,7 +37,7 @@ NodeOUs:
     OrganizationalUnitIdentifier: orderer'> $FABRIC_CA_CLIENT_HOME/msp/config.yaml
 
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/org-ca/ca-cert.pem
-export FABRIC_CA_CLIENT_HOME=$PWD/certs/admin/org-ca/
+export FABRIC_CA_CLIENT_HOME=$PWD/certs/peerorg/
 set -x
 fabric-ca-client enroll -d -u https://admin:adminpw@0.0.0.0:7055
 fabric-ca-client register -d --id.name peer-org --id.secret peerpw --id.type peer -u https://0.0.0.0:7055
@@ -62,7 +62,7 @@ NodeOUs:
     OrganizationalUnitIdentifier: orderer' >  $FABRIC_CA_CLIENT_HOME/msp/config.yaml
 
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/ord-ca/ca-cert.pem
-export FABRIC_CA_CLIENT_HOME=$PWD/certs/admin/ord-ca/
+export FABRIC_CA_CLIENT_HOME=$PWD/certs/ordererorg/
 set -x
 fabric-ca-client enroll -d -u https://admin:adminpw@0.0.0.0:7056
 fabric-ca-client register -d --id.name orderer --id.secret ordererpw --id.type orderer -u https://0.0.0.0:7056
@@ -83,3 +83,41 @@ NodeOUs:
   OrdererOUIdentifier:
     Certificate: cacerts/0-0-0-0-7056.pem
     OrganizationalUnitIdentifier: orderer' > $FABRIC_CA_CLIENT_HOME/msp/config.yaml
+
+echo "####### ENROLLING ORDERER #########"
+
+export FABRIC_CA_CLIENT_HOME=$PWD/certs/ordererorg/orderer
+set -x
+fabric-ca-client enroll -d -u https://orderer:ordererpw@0.0.0.0:7056
+set +x
+
+export FABRIC_CA_CLIENT_MSPDIR=msp
+echo "moving the config file"
+cp $PWD/certs/ordererorg/msp/config.yaml $FABRIC_CA_CLIENT_HOME/msp/config.yaml
+
+echo "changing the root ca"
+export FABRIC_CA_CLIENT_TLS_CERTFILES=$PWD/ca/tls-ca/ca-cert.pem
+
+export FABRIC_CA_CLIENT_MSPDIR=tls
+set -x
+fabric-ca-client enroll -d -u https://orderer:ordererpw@0.0.0.0:7054 --enrollment.profile tls --csr.hosts orderer --csr.hosts localhost
+set +x
+
+cp  $FABRIC_CA_CLIENT_HOME/tls/tlscacerts/* $FABRIC_CA_CLIENT_HOME/tls/ca.crt
+cp  $FABRIC_CA_CLIENT_HOME/tls/signcerts/* $FABRIC_CA_CLIENT_HOME/tls/server.crt
+cp  $FABRIC_CA_CLIENT_HOME/tls/keystore/* $FABRIC_CA_CLIENT_HOME/tls/server.key
+
+echo "editing the ord-ca admin tlscerts "
+
+mkdir $PWD/certs/ordererorg/msp/tlscacerts
+cp $FABRIC_CA_CLIENT_HOME/tls/ca.crt $PWD/certs/ordererorg/msp/tlscacerts/ca.cert
+
+echo "editing the orderer root tlsca directory"
+
+mkdir $PWD/certs/ordererorg/tlsca
+cp $FABRIC_CA_CLIENT_HOME/tls/ca.crt $PWD/certs/ordererorg/tlsca/tls-ca.crt
+
+echo "editing root ca directory"
+
+mkdir $PWD/certs/ordererorg/ca
+cp $FABRIC_CA_CLIENT_HOME/msp/cacerts/* $PWD/certs/ordererorg/ca/orderer-ca.cert

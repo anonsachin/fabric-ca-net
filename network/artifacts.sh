b481f68ae32721db95860a851ee080fe2c72526e
sachin-ngpws <sachin@ngpwebsmart.com>
2020-09-30 16:22:06 +0530

    added anchor peer update tx

--- network/artifacts.sh
+++ network/artifacts.sh
@@ -16,11 +16,20 @@ function genesis() {
 }
 
 function channel() {
-        echo "#########  Generating Channel Creation Tx ##############"
+    echo "#########  Generating Channel Creation Tx ##############"
 	set -x
  	configtxgen -profile OrgChannel -outputCreateChannelTx ./channel-artifacts/${CHANNEL_NAME}.tx -channelID $CHANNEL_NAME
 	set +x
-}	
+}
+
+function anchorPeer() {
+	ORG=$1
+	echo "#########  Generating Anchor Peer Update Tx ##############"
+	set -x
+ 	configtxgen -profile OrgChannel -outputAnchorPeersUpdate ./channel-artifacts/${ORG}.tx -channelID $CHANNEL_NAME -asOrg $ORG
+	set +x
+}
 
 genesis
 channel
+anchorPeer orgMSP

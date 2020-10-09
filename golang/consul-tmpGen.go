74ebdf4fd661b336902fe440372259f0f932bebc
sachin-ngpws <sachin@ngpwebsmart.com>
2020-10-09 17:10:23 +0530

    created folder structure

--- golang/consul-tmpGen.go
+++ golang/consul-tmpGen.go
@@ -3,23 +3,29 @@ package main
 import (
 	"strings"
 	"fmt"
+	"path/filepath"
+	"os"
 )
 
 //TemplateGen edits the consul template
-func TemplateGen(a string,cert string) string{
+func TemplateGen(a string,cert string, role string) string{
 	a = strings.Replace(a,"ORG","peer",1)
-	a = strings.Replace(a,"ROLE","peer",1)
+	a = strings.Replace(a,"ROLE",role,1)
 	a = strings.Replace(a,"CNAME","peer.service.consul",2)
 	a = strings.Replace(a,"TTL","24h",1)
 	a = strings.Replace(a,"CERT",cert,1)
 	return a
 }
 //ConsulTempGen Generate template
-func ConsulTempGen() {
-	a := "{{ with secret \"ORGCA/issue/ROLE\" \"common_name=CNAME\" \"ttl=TTL\" \"alt_names=localhost,CNAME\" \"ip_sans=127.0.0.1\"}}\n{{ .Data.CERT }}\n{{ end }}"
-	fmt.Printf("The BASE Versin ==> %s \n",a)
+func ConsulTempGen(a string,outDir string, role string) {
+	// a := "{{ with secret \"ORGCA/issue/ROLE\" \"common_name=CNAME\" \"ttl=TTL\" \"alt_names=localhost,CNAME\" \"ip_sans=127.0.0.1\"}}\n{{ .Data.CERT }}\n{{ end }}"
+	fmt.Printf("The BASE Versin ==> \n %s \n",a)
 	certs := []string{"issuing_ca","private_key","certificate"}
-	for _,cert := range certs {
-		fmt.Printf("The NEW Versin ==> %v \n",TemplateGen(a,cert))
+	destinations := []string{"msp/cacerts","msp/keystore","msp/signcerts"}
+	var path string
+	for i := range certs {
+		path = filepath.Join(outDir,role,destinations[i])
+		os.MkdirAll(path,os.ModePerm)
+		fmt.Printf("The NEW Versin ==> \n %v:%v \n",path,TemplateGen(a,certs[i],role))
 	}
 }
--- golang/main.go
+++ golang/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"flag"
 )
 
 func main() {
-	fmt.Println("Hello, playground")
-	ConsulTempGen()
-}
 
+	tmpFile := flag.String("template","template.tpl","Used to get the template file")
+	outDir := flag.String("out_dir","./NewOrg/","Used to get the directory for certs of the new org")
+	role := flag.String("role","peer","Used to specify the role of the certs")
+	flag.Parse()
+	fmt.Printf("The template file from %s \n",*tmpFile)
+	file, err := ioutil.ReadFile(*tmpFile)
+	if err != nil {
+		panic("The file error ==> "+err.Error())
+	}
+	ConsulTempGen(string(file),*outDir,*role)
+}
